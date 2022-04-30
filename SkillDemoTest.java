044d822c4424fd0cb795f017c98483f7b77dfe5f
Kailey Wong <kaileyswong@gmail.com>
2022-04-30 15:43:08 -0700

    fix test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void testMultiply(){
-        assertEquals(4, SkillDemo.multiply(1, 2));
+        assertEquals(2, SkillDemo.multiply(1, 2));
     }
 }
